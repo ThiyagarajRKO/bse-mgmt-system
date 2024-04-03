e5fd0ca4667e2140b88aad6f0f0ff826abcbc3a1
Thiyagaraj <ThiyagarajRKO@gmail.com>
2024-04-03 18:37:05 +0530

    update pending

--- src/controllers/procurement_lots.js
+++ src/controllers/procurement_lots.js
@@ -259,6 +259,7 @@ export const GetStats = ({
       const procurementRows = await models.ProcurementLots.findAll({
         subQuery: false,
         attributes: [
+          "id",
           "procurement_date",
           "procurement_lot",
           [
