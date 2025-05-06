a75244996551c93ebae4f3eeb5fdaf9ac183c59f
Thiyagaraj <ThiyagarajRKO@gmail.com>
2025-05-06 12:55:48 +0530

    fixed migration

---
+++ migrations/20240401141437-create-procurement_lots.js
@@ -0,0 +1,75 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("procurement_lots", {
+      id: {
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      procurement_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      procurement_lot: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      unit_master_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        onDelete: "RESTRICT",
+        onUpdate: "CASCADE",
+        references: {
+          model: { tableName: "unit_master" },
+          key: "id",
+        },
+      },
+      is_active: {
+        defaultValue: false,
+        type: Sequelize.BOOLEAN,
+      },
+      created_at: {
+        defaultValue: Sequelize.fn("now"),
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+      },
+      created_by: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        onDelete: "RESTRICT",
+        onUpdate: "CASCADE",
+        references: {
+          model: { tableName: "user_profiles" },
+          key: "id",
+        },
+      },
+      updated_by: {
+        type: Sequelize.UUID,
+        onDelete: "RESTRICT",
+        onUpdate: "CASCADE",
+        references: {
+          model: { tableName: "user_profiles" },
+          key: "id",
+        },
+      },
+      deleted_by: {
+        type: Sequelize.UUID,
+        onDelete: "RESTRICT",
+        onUpdate: "CASCADE",
+        references: {
+          model: { tableName: "user_profiles" },
+          key: "id",
+        },
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("procurement_lots");
+  },
+};
