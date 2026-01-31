40e286c69e202c4ec42f06360885bf4cac2b5c5a
Mithra <mithragopikrishnan@gmail.com>
2026-02-01 00:32:53 +0530

    add order to purchase inventory

---
+++ migrations/20260128-add-order-id-to-purchase-inventory.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn("purchase_inventory", "order_id", {
+      type: Sequelize.UUID,
+      allowNull: true,
+      onDelete: "RESTRICT",
+      onUpdate: "CASCADE",
+      references: {
+        model: { tableName: "orders" },
+        key: "id",
+      },
+    });
+
+    await queryInterface.addIndex("purchase_inventory", ["order_id"], {
+      name: "purchase_inventory_order_id_idx",
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeIndex(
+      "purchase_inventory",
+      "purchase_inventory_order_id_idx",
+    );
+    await queryInterface.removeColumn("purchase_inventory", "order_id");
+  },
+};
